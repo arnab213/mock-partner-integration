31767cae2cd220d52dc4d19886ca0aaae62e50a9
ARNAB GUIN <88367704+arnab213@users.noreply.github.com>
2026-06-15 18:58:57 +0530

    add MockPartnerIntegrationApplication.java

---
+++ src/main/java/com/arnab/mockpartnerintegration/MockPartnerIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.arnab.mockpartnerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPartnerIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockPartnerIntegrationApplication.class, args);
+    }
+}
